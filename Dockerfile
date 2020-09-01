e1e16cb5bdf6cb5a6ab0c3bc8e500f55f1168ac1
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-09-01 12:37:39 +0300

    Try to fix Preload EN stanza resources process
    Try to fix Preload EN stanza resources process

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ RUN apt-get -y clean \
     && apt-get -y install python-dev \
     && apt-get -y install build-essential \
     && apt-get -y install curl \
+    && apt-get -y install wget \
+    && apt-get -y install unzip \
     && curl https://getmic.ro | bash \
     # ------------------------------------------------------------------
     && pip install -r ./deploy/requirements.txt --src /usr/local/src \
@@ -19,9 +21,16 @@ RUN apt-get -y clean \
     && apt-get -y clean \
     && apt-get -y autoremove
 
+# Preload EN stanza resources
+RUN wget https://raw.githubusercontent.com/stanfordnlp/stanza-resources/master/resources_1.1.0.json -P ~/stanza_resources \
+    wget https://nlp.stanford.edu/software/stanza/1.1.0/nb/default.zip -P ~/stanza_resources/nb \
+    unzip ~/stanza_resources/nb/default.zip -d ~/stanza_resources/nb/ \
+    rm ~/stanza_resources/nb/default.zip \
+    mv ~/stanza_resources/resources_1.1.0.json ~/stanza_resources/resources.json
+
 COPY ./deploy/nginx.conf /etc/nginx
 
-RUN chmod -R a+rwx /stanza/serv
+# RUN chmod -R a+rwx /stanza/serv
 
 RUN chmod +x ./start.sh
 CMD ["./start.sh"]
--- server.py
+++ server.py
@@ -46,13 +46,18 @@ app.secret_key = os.urandom(42)
 # Init Stanza (load models too)
 import stanza
 
+# try:
+#     snlp = stanza.Pipeline(lang="nb", processors='tokenize,mwt,pos,lemma', dir='./deploy/stanza_resources')
+# except:
+#     logging.debug('Installing Stanza pretrained NLP model for Norwegian Bokmaal.')
+#     stanza.download('nb', dir='./deploy/stanza_resources')
+#     logging.debug('Stance pretrained NLP model for Norwegian Bokmaal is ready to use.')
+#     snlp = stanza.Pipeline(lang="nb", processors='tokenize,mwt,pos,lemma', dir='./deploy/stanza_resources')
+
 try:
-    snlp = stanza.Pipeline(lang="nb", processors='tokenize,mwt,pos,lemma', dir='./deploy/stanza_resources')
-except:
-    logging.debug('Installing Stanza pretrained NLP model for Norwegian Bokmaal.')
-    stanza.download('nb', dir='./deploy/stanza_resources')
-    logging.debug('Stance pretrained NLP model for Norwegian Bokmaal is ready to use.')
-    snlp = stanza.Pipeline(lang="nb", processors='tokenize,mwt,pos,lemma', dir='./deploy/stanza_resources')
+    snlp = stanza.Pipeline(lang="nb", processors='tokenize,mwt,pos,lemma')
+except Exception as e:
+    logging.error(traceback.format_exc())
 
 @app.route('/')
 def index():
